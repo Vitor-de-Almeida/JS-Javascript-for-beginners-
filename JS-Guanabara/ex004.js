191d400a3d35f0757fd9fa1951e80737f67f2942
Vitor Renan <vitorrenanalmeida@gmail.com>
2023-11-12 21:01:52 -0300

    Daily Commit

---
+++ JS-Guanabara/ex004.js
@@ -0,0 +1,16 @@
+// let n = 1
+// while (n < 6) {
+//     console.log('Tudo bem?')
+//     n++
+// }
+
+// let n = 1
+// do {
+//     console.log('Tudo bem?')
+//     n++
+// } while (n < 6)
+
+console.log('Vai começar!')
+for (n; n <= 10; n++) {
+    console.log(n)
+}
