abc504fa6e066e9d39528d28c69abd1a1dc73c37
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-03 22:43:17 -0300

    Daily Commit

---
+++ JS-One-Bit-Code/aula051/array-hof.js
@@ -0,0 +1,88 @@
+const personagens = [
+    { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+    { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+    { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+    { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+    { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+  ]
+
+  // Map
+
+//   const nomes = personagens.map(function(personagem){
+//     return personagem.nome
+    
+//   })
+
+//   const racas = personagens.map(function(origem){
+//     return origem.raca
+    
+//   })
+
+//   for (let i = 0; i < personagens.length; i++) {
+//     nomes.push(personagens[i].nome)
+// }
+
+//console.log(nomes)
+//console.log(racas)
+
+// Filter
+
+// const orcs = []
+
+// for (let i = 0; i < personagens.length; i++) {
+//     if (personagens[i].raca === "Orc") {
+//         orcs.push(personagens[i])
+//     }
+// }
+
+// const orcs = personagens.filter(function (personagem) {
+//     return personagem.raca === "Orc"
+// })
+
+//console.log(orcs)
+
+
+// Reduce
+
+// let soma = 0
+
+// for (let i = 0; i < personagens.length; i++) {
+//     soma += personagens[i].nivel
+//  }
+
+//  console.log(soma)
+
+// const nivelTotal = personagens.reduce(function (valorAcumulado, personagem) {
+//     return valorAcumulado + personagem.nivel
+// }, 0)
+
+// console.log(nivelTotal)
+
+// const racas = personagens.reduce(function (valorAcumulado, personagem) {
+//     if (valorAcumulado[personagem.raca]) {
+//         valorAcumulado[personagem.raca].push(personagem)
+//     } else {
+//         valorAcumulado[personagem.raca] = [personagem]
+//     }
+//     return valorAcumulado
+// }, {})
+
+// console.log(racas)
+
+const racas = {}
+
+for (let i = 0; i < personagens.length; i++) {
+
+    const personagem = personagens[i]
+    console.log(personagem)
+    
+    if (racas[personagem.raca]) {
+        racas[personagem.raca].push(personagem);
+    } else {
+        racas[personagem.raca] = [personagem];
+    }
+}
+
+console.log(racas)
