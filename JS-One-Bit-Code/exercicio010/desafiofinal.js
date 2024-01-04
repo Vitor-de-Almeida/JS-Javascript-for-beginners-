e4c71240812fb410b0796e5a063804ff4a22cd2f
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-04 02:02:32 -0300

    Daily Commit

---
+++ JS-One-Bit-Code/exercicio010/desafiofinal.js
@@ -0,0 +1,41 @@
+let opcao = 0
+
+do {
+    opcao = Number(prompt("Bem Vindo ao sistema gerenciador de vagas de emprego do google!" + 
+    "\nSelecione a opção desejada:" + 
+    "\n1. Listar vagas disponíveis" + 
+    "\n2. Criar uma nova vaga" + 
+    "\n3. Visualizar uma vaga" + 
+    "\n4. Inscrever um candidato em uma vaga" + 
+    "\n5. Excluir uma vaga" + 
+    "\n6. Sair"))
+
+    switch (opcao) {
+        case 1:
+
+        break
+
+        case 2:
+
+        break
+
+        case 3:
+
+        break
+
+        case 4:
+
+        break
+
+        case 5:
+
+        break
+
+        default:
+        alert("Opção Inválida, tente novamente!")
+        break
+        
+    }
+
+
+} while (opcao != 6)
