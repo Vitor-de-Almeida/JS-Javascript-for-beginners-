f55f016d795d071549c24a4ed0a4fc28d6f719d8
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-04 02:29:07 -0300

    Daily Commit

--- JS-One-Bit-Code/exercicio010/desafiofinal.js
+++ JS-One-Bit-Code/exercicio010/desafiofinal.js
@@ -1,4 +1,15 @@
 let opcao = 0
+let soma = 0
+
+vagaDisponivel = []
+
+function exibirVagasDeEmprego (nomeDaVaga, indice, quantidadeDeVagas) {
+    console.log({
+        nomeDaVaga,
+        indice,
+        quantidadeDeVagas
+    })
+}
 
 do {
     opcao = Number(prompt("Bem Vindo ao sistema gerenciador de vagas de emprego do google!" + 
@@ -12,7 +23,16 @@ do {
 
     switch (opcao) {
         case 1:
+            if (vagaDisponivel.length > 0) {
 
+                for (let i = 0; i < vagaDisponivel.length; i++) {
+                    soma += vagaDisponivel[i].quantidadeDeVagas
+                    alert(exibirVagasDeEmprego(i, vagaDisponivel[i], soma))
+                    } 
+                    
+                } else {
+                        alert("Não há vagas registradas")
+                }   
         break
 
         case 2:
