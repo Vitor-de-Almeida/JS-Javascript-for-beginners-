d5f987686b30cdb6e5982005dd3a8c9ed9178555
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-07 03:52:00 -0300

    Daily Commit

--- JS-One-Bit-Code/js-dom-III/exercicio001-escalandoJogador/escalandoJogador.js
+++ JS-One-Bit-Code/js-dom-III/exercicio001-escalandoJogador/escalandoJogador.js
@@ -2,32 +2,41 @@ function escalarJogador() {
 
     const posJogador = window.document.querySelector('input#posicao').value
     const nomeJogador = window.document.querySelector('input#nome').value
-    const numeroJogador = window.document.querySelector('input#numero')
-    const numeroJogadorValor = Number(numeroJogador.value)
+    const numeroJogador = window.document.querySelector('input#numero').value
+
+    const confirmacao = confirm ('Escalar o ' + nomeJogador + ' como ' + posJogador + '?')
+
+    if (confirmacao) {
 
     const jogadorUl = window.document.getElementById('jogadoresEscalados')
     const jogadorLi = window.document.createElement('li')
-    jogadorLi.id = numeroJogadorValor
-
-    const confirmacao = confirm ('O jogador escalado joga na posição ' +  posJogador + ', conhecido pelo nome de ' +  nomeJogador + ', usando a camisa de número ' +  numeroJogadorValor + '.')
-
-    if (confirmacao) {jogadorLi.innerHTML = 'O jogador escalado joga na posição ' +  posJogador + ', conhecido pelo nome de ' +  nomeJogador + ', usando a camisa de número ' +  numeroJogadorValor + '.' 
-
+    jogadorLi.id = numeroJogador
     alert('Jogador incluído com sucesso')
-    }
-
+    jogadorLi.innerHTML = ('O jogador escalado joga na posição ' +  posJogador + ', conhecido pelo nome de ' +  nomeJogador + ', usando a camisa de número ' +  numeroJogador + '.') 
     jogadorUl.appendChild(jogadorLi)
+
+    window.document.querySelector('input#posicao').value = ''
+    window.document.querySelector('input#nome').value = ''
+    window.document.querySelector('input#numero').value = ''
+    }
 }
 
 function removerJogador() {
 
-    const RemoverJogadorUl = window.document.getElementById('jogadoresEscalados')
     const selecionarJogador = window.document.querySelector('input#remover').value
     const jogadorLi = window.document.getElementById(selecionarJogador)
 
-    if (jogadorLi) {
-        RemoverJogadorUl.removeChild(jogadorLi) 
+    const confirmacao = confirm('Remover o jogador?')
+
+    if (confirmacao) {
+        if (jogadorLi) {
+            window.document.getElementById('jogadoresEscalados').removeChild(jogadorLi) 
+            window.document.querySelector('input#remover').value = '' 
+        } else {
+            alert('Número de camisa inválido ou jogador não encontrado')
+        }
+        
     } else {
-        alert('Número de camisa inválido ou jogador não encontrado')
+        alert('Remoção cancelada')
     }
 }
