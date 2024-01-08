81696ee805eb80c037f6e36ff628b5a59626ebec
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-07 22:03:28 -0300

    Daily Commit

---
+++ JS-One-Bit-Code/js-dom-III/eventos-1-js/index.js
@@ -0,0 +1,33 @@
+function register (ev) {
+    console.log(ev)
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation) {
+        alert("Usuário " + username + " registrado!")
+      } else {
+        alert("As senhas não conferem")
+      }
+}
+
+const button = document.getElementById('register-button')
+
+button.addEventListener('click', register)
+
+// document.getElementById('register-button').addEventListener('click', register)
+
+function removeListener() {
+    button.removeEventListener('click', register)
+    alert('Evento removido')
+}
+
+button.addEventListener('mouseover', function (ev) {
+    console.log(ev.target)
+})
+
+function removeListener () {
+    alert('Olá')
+}
+
