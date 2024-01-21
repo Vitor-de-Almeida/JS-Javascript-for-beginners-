f82f76bef14982ddf921f520a1dcd70ad9cfca44
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-21 03:58:28 -0300

    Daily Commit

--- jsDaveYouTube/objects/objects.js
+++ jsDaveYouTube/objects/objects.js
@@ -24,6 +24,74 @@
 
 //---------------------------------
 
-const 
+// const vehicle = {
+//     wheels: 4,
+//     engine: function() {
+//         return "Vrroooom!"
+//     }
+// }
+
+// const truck = Object.create(vehicle)
+// truck.doors = 2
+
+// console.log(truck)
+// console.log(truck.wheels) //Inheritance
+// console.log(truck.engine())
+
+// const car = Object.create(vehicle)
+// car.doors = 4;
+// car.engine = function () {
+//     return "Whoooosh!"
+// }
+
+// console.log(car.engine())
+// console.log(car.wheels)
+
+// const tesla = Object.create(car)
+
+// console.log(tesla.wheels)
+// console.log(tesla.engine())
+
+// tesla.engine = function () {
+//     return "Shhhh..."
+// }
+
+// console.log(tesla.engine())
+
+//---------------------------------
+
+// const myName = {name:"Vitor"}
+
+// myName.age = 2
+
+// myName.profession = [{
+//     homeoffice: "enginner", 
+//     present: "company"
+// }, "devoloper"]
+
+// console.log(myName.profession[0].homeoffice)
+
+const band = {
+    vocals: "Robert Plant",
+    guitar: "Jimmy Page",
+    bass: "John Paul Jones",
+    drums: "John Bonham"
+}
+
+delete band.drums;
+console.log(band.hasOwnProperty("drums"))
+
+console.log(band)
+
+console.log(Object.keys(band))
+console.log(Object.values(band))
+
+const singers = []
+
+for (let key in band) {
+    console.log(`On ${key}, it's ${band[key]}`)
+    singers.push(band[key])
+}
+
 
 
