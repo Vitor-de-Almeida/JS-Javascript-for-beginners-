53805e7d0783f0650636005df9fb63781b6f7641
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-01-22 23:56:39 -0300

    Daily Commit

--- jsDaveYouTube/classes/classes.js
+++ jsDaveYouTube/classes/classes.js
@@ -1,30 +1,30 @@
 class Pizza {
-    constructor(pizzaType, pizzaSize) {
-        this.type = pizzaType
+    constructor(pizzaSize) {
         this.size = pizzaSize
         this.crust = "original"
-        this.toppings = []
     }
+
     getCrust()  {
         return this.crust
     }
     setCrust(pizzaCrust) {
         this.crust = pizzaCrust
     }
-    getToppings () {
-        return this.toppings
-    }
-    setToppings(topping) {
-        this.toppings.push(topping)
-    }
-    bake () {
-        console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`)
-    }
 }
 
-const myPizza = new Pizza('pepperoni', 'small')
+// const myPizza = new Pizza('pepperoni', 'small')
 
-myPizza.setCrust("thin")
-myPizza.bake()
+// myPizza.setCrust("thin")
+// myPizza.bake()
+// myPizza.setToppings("sausage")
+// myPizza.setToppings("olives")
 
-console.log(myPizza.getCrust())
+// console.log(myPizza.getCrust())
+// console.log(myPizza.getToppings())
+
+class SpecialtyPizza extends Pizza {
+    constructor(pizzaSize){
+        super(pizzaSize)
+        this.type = "The Works"
+    }
+}
