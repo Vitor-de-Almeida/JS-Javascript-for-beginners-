d82ad52e5ab110e191e95ee547657db2deb4de95
Vitor Renan <vitorrenanalmeida@gmail.com>
2024-04-16 09:48:13 -0300

    Daily Commit

--- JS-One-Bit-Code/js-dom-III/exercicio004-jogoDaVelha/jogoDaVelha.js
+++ JS-One-Bit-Code/js-dom-III/exercicio004-jogoDaVelha/jogoDaVelha.js
@@ -1,4 +1,43 @@
-const playerOneName = window.document.getElementById("container-player-one")
-const playerTwoName = window.document.getElementById("container-player-two")
+let playerOneName = window.document.getElementById("namePlayerOne")
+let playerTwoName = window.document.getElementById("namePlayerTwo")
 
-function
+let currentPlayer = 'X'
+
+const buttonPlayersName = window.document.getElementById('buttonPlayersName')
+const buttonText = window.document.getElementById('table-title')
+
+const XO = window.document.querySelectorAll('.charKey')
+
+
+buttonPlayersName.addEventListener('click', function () {
+    playerOneName = playerOneName.value
+    playerTwoName = playerTwoName.value
+    buttonText.innerText = 'É a vez do jogador: ' + playerOneName
+    playGame()
+})
+
+
+
+function playGame () {
+    
+    XO.forEach(function(clickButtonCell) {
+        clickButtonCell.addEventListener('click', function() {
+        
+            if (currentPlayer === 'X' && clickButtonCell.innerText == '') {
+                clickButtonCell.innerText = 'X'
+                clickButtonCell.dataset.value = 1
+                currentPlayer = 'O'
+                buttonText.innerText = 'É a vez do jogador: ' + playerTwoName
+                if ()
+                
+            } else if (currentPlayer === 'O' && clickButtonCell.innerText == '') {
+                clickButtonCell.innerText = 'O'
+                clickButtonCell.dataset.value = -1
+                currentPlayer = 'X'  
+                buttonText.innerText = 'É a vez do jogador: ' + playerOneName
+            }
+    
+            })
+    }) 
+
+}
